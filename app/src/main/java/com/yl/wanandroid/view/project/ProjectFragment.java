7cff0e15866b96ef71e1851fa0cf26cfb538b156
LeeYang <lee.yang@d8aspring.com>
2018-11-07 12:00:54 +0800

    apply fab to project list

--- app/src/main/java/com/yl/wanandroid/view/project/ProjectFragment.java
+++ app/src/main/java/com/yl/wanandroid/view/project/ProjectFragment.java
@@ -1,11 +1,14 @@
 package com.yl.wanandroid.view.project;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.View;
 
 import com.yl.wanandroid.R;
 import com.yl.wanandroid.base.ViewPagerAdapter;
@@ -20,12 +23,17 @@ import java.util.List;
 import butterknife.BindView;
 
 public class ProjectFragment extends BaseMvpFragment<ProjectView, ProjectPresenter> implements ProjectView {
+    @BindView(R.id.abl_project)
+    AppBarLayout ablProject;
     @BindView(R.id.tb_project)
     Toolbar tbProject;
     @BindView(R.id.tl_project)
     TabLayout tlProject;
     @BindView(R.id.vp_project)
     ViewPager vpProject;
+    @BindView(R.id.fab_to_top)
+    FloatingActionButton fabToTop;
+    private List<Fragment> fragments;
 
     @Override
     protected int getLayoutId() {
@@ -36,6 +44,19 @@ public class ProjectFragment extends BaseMvpFragment<ProjectView, ProjectPresent
     protected void initView(Bundle arguments) {
         tbProject.inflateMenu(R.menu.menu_toolbar_home);
         tlProject.setTabMode(TabLayout.MODE_SCROLLABLE);
+    }
+
+    @Override
+    protected void initListener() {
+        fabToTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProjectListFragment currentFragment =
+                        (ProjectListFragment) fragments.get(vpProject.getCurrentItem());
+                currentFragment.rvProject.smoothScrollToPosition(0);
+                ablProject.setExpanded(true, true);
+            }
+        });
     }
 
     @Override
@@ -51,7 +72,7 @@ public class ProjectFragment extends BaseMvpFragment<ProjectView, ProjectPresent
     @Override
     public void showProjectCategory(List<ProjectCategory> list) {
         List<String> categories = new ArrayList<>();
-        List<Fragment> fragments = new ArrayList<>();
+        fragments = new ArrayList<>();
         for (ProjectCategory category : list) {
             categories.add(Html.fromHtml(category.getName()).toString());
             fragments.add(ProjectListFragment.newInstance(category.getId()));
