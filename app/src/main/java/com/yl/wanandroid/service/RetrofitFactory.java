b7ccc600d1d0944851de4c1fafa804569488ae2e
yangli <m15056946093@163.com>
2018-11-12 23:41:30 +0800

    clean code and add todo for RetrofitFactory

--- app/src/main/java/com/yl/wanandroid/service/RetrofitFactory.java
+++ app/src/main/java/com/yl/wanandroid/service/RetrofitFactory.java
@@ -1,17 +1,15 @@
 package com.yl.wanandroid.service;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
-import com.yl.wanandroid.service.interfaces.APIService;
+import com.yl.wanandroid.BuildConfig;
 import com.yl.wanandroid.app.Constant;
+import com.yl.wanandroid.service.interfaces.APIService;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -22,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RetrofitFactory {
-    //private static final String TAG = "RetrofitFactory";
+    private static final String TAG = "RetrofitFactory";
     private static final int TIME_OUT = 5;
     private static APIService apiService;
     private static RetrofitFactory retrofitFactory;
@@ -31,29 +29,18 @@ public class RetrofitFactory {
         HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
             @Override
             public void log(@NonNull String message) {
-                //Log.e(TAG, "log: " + message);
+                Log.e(TAG, "log: " + message);
             }
         });
-        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        /*
-         * 由于retrofit底层的实现是通过OKHttp实现的，所以可以通过OKHttp来设置一些连接参数
-         * 如超时等
-         */
+        logInterceptor.setLevel(BuildConfig.DEBUG ?
+                HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+        // 可以通过OkHttp来设置一些连接参数
         OkHttpClient.Builder client = new OkHttpClient().newBuilder();
         client.addInterceptor(logInterceptor)
-                .addInterceptor(new Interceptor() {
-                    @Override
-                    public Response intercept(@NonNull Chain chain) throws IOException {
-                        Request request = chain.request();
-                        return chain.proceed(request);
-                    }
-                })
                 .cookieJar(AuthCookies.getInstance())
                 .connectTimeout(TIME_OUT, TimeUnit.SECONDS);
-        /*
-         * 构造函数私有化
-         * 并在构造函数中进行retrofit的初始化
-         */
+        // TODO: 18-11-12 研究 Interceptor 和 Retrofit 以优化网络框架
+        // 构造 Retrofit
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constant.BASE_URL)
                 .client(client.build())
