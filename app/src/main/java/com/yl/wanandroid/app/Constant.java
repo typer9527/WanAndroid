eb3c8e75287e88bc970ce2a37f27869dec5ac1ff
lee <m15056946093@163.com>
2019-03-16 14:24:06 +0800

    change http to https

--- app/src/main/java/com/yl/wanandroid/app/Constant.java
+++ app/src/main/java/com/yl/wanandroid/app/Constant.java
@@ -1,7 +1,7 @@
 package com.yl.wanandroid.app;
 
 public class Constant {
-    public static final String BASE_URL = "http://www.wanandroid.com";
+    public static final String BASE_URL = "https://www.wanandroid.com";
     public static final String KEY_IS_LOGON = "key_is_logon";
     public static final String KEY_LOCAL_COOKIE = "key_local_cookie";
     public static final String KEY_CATEGORY_ID = "key_category_id";
