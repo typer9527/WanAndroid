78bf49ff19e7c301b4d7c00f6a993b33a8b0e9b3
lee <m15056946093@163.com>
2019-03-16 17:13:39 +0800

    add view of system

---
+++ app/src/main/java/com/yl/wanandroid/view/system/SystemFragment.java
@@ -0,0 +1,6 @@
+package com.yl.wanandroid.view.system;
+
+import android.support.v4.app.Fragment;
+
+public class SystemFragment extends Fragment {
+}
---
+++ app/src/main/java/com/yl/wanandroid/view/system/SystemView.java
@@ -0,0 +1,10 @@
+package com.yl.wanandroid.view.system;
+
+import com.yl.wanandroid.base.BaseView;
+import com.yl.wanandroid.service.dto.Articles;
+import com.yl.wanandroid.service.dto.ProjectCategory;
+
+import java.util.List;
+
+public interface SystemView extends BaseView {
+}
